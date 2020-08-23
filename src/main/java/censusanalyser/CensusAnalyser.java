694b8eb44300c21ef7dd1f193b2ed368780de7e4
sujit gargate <sujitgargate5893@gmail.com>
2020-08-24 04:58:37 +0530

    Refactored Code To Support List Insted Of Iterator

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -7,6 +7,7 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
@@ -15,8 +16,8 @@ public class CensusAnalyser {
    public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException, CSVException {
       try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
          ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-         Iterator<IndiaCensusCSV> censusCSVIterator = csvBuilder.getCSVFileIterator(reader, IndiaCensusCSV.class);
-         return getCount(censusCSVIterator);
+         List <IndiaCensusCSV> censusCSVList = csvBuilder.getCSVList(reader, IndiaCensusCSV.class);
+         return censusCSVList.size();
       } catch (IOException e) {
          throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
       } catch (RuntimeException e) {
